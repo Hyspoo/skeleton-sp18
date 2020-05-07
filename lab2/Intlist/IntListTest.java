34dcfa5988e104b60bd383646d6b18556af2a1bd
Sangpu Yang <thumper.yang@gmail.com>
2020-05-07 18:05:12 +0800

    no message

--- lab2/Intlist/IntListTest.java
+++ lab2/Intlist/IntListTest.java
@@ -66,4 +66,9 @@ public class IntListTest {
         assertEquals(IntList.of(1, 2, 3), A);
     }
 
+    public static void main(String[] args) {
+        IntListTest a = new IntListTest();
+        a.testCatenate();
+        a.testDcatenate();
+    }
 }
