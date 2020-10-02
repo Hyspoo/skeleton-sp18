7a9b044f7c91e5350dd05694b74933892e5ac5a5
Sangpu Yang <thumper.yang@gmail.com>
2020-10-02 21:07:15 +0800

    add buildRoom

--- proj2/byog/Core/FirstWorldGenerator.java
+++ proj2/byog/Core/FirstWorldGenerator.java
@@ -3,6 +3,7 @@ package byog.Core;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
+import byog.lab5.HexWorld;
 
 import java.util.Random;
 
@@ -10,10 +11,12 @@ public class FirstWorldGenerator {
 
     private static final int WIDTH = 100;
     private static final int HEIGHT = 50;
-    private static final int SIZE = 2;
-    //private static final double inheritRate = 0.5;
 
-    private static final long SEED = 100;
+    private static final int MINSIZE = 3;
+    private static final int MAXSIZEX = 14;
+    private static final int MAXSIZEY = 10;
+
+    private static final long SEED = 1;
     private static final Random RANDOM = new Random(SEED);
 
     private static class Point {
@@ -23,6 +26,43 @@ public class FirstWorldGenerator {
             x = a;
             y = b;
         }
+    }
+
+    private static void buildRoom(TETile[][] tiles, Point leftBottom, Point rightTop) {
+        for (int i = leftBottom.x; i <= rightTop.x; i += 1) {
+            tiles[i][leftBottom.y] = Tileset.WALL;
+            tiles[i][rightTop.y] = Tileset.WALL;
+        }
+        for (int i = leftBottom.y; i <= rightTop.y; i += 1) {
+            tiles[leftBottom.x][i] = Tileset.WALL;
+            tiles[rightTop.x][i] = Tileset.WALL;
+        }
+        for (int i = leftBottom.x + 1; i < rightTop.x; i += 1) {
+            for (int j = leftBottom.y + 1; j < rightTop.y; j += 1) {
+                tiles[i][j] = Tileset.FLOOR;
+            }
+        }
+    }
+
+    
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        Point P1 = new Point(20,20);
+        Point P2 = new Point(40,40);
+
+        buildRoom(world, P1, P2);
+
+        ter.renderFrame(world);
     }
 
 }
